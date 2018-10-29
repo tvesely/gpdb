apache/hawq

HAWQ-1600. Parquet table data vectorized scan

contrib/vexecutor/vexecutor.c
@@ -189,7 +189,7 @@ static TupleTableSlot* VExecProcNode(PlanState *node)
         case T_ParquetScanState:
         case T_AppendOnlyScanState:
         case T_TableScanState:
-            result = ExecTableVScan((TableScanState*)node);
+			result = ExecTableVScanVirtualLayer((TableScanState*)node);
             break;
         default:
             break;
@@ -208,10 +208,6 @@ static bool VExecEndNode(PlanState *node)
 	{
 		case T_AppendOnlyScanState:
 		case T_ParquetScanState:
-            tbDestroy((TupleBatch *) (&node->ps_ResultTupleSlot->PRIVATE_tb));
-            tbDestroy((TupleBatch *) (&((TableScanState *) node)->ss.ss_ScanTupleSlot->PRIVATE_tb));
-			ret = true;
-			break;
 		case T_TableScanState:
 			ExecEndTableScan((TableScanState *)node);
 			ret = true;

tvesely/gpdb

(cherry picked from commit cfee2c51d7b28d23c644e903d544b3645c42b9a9)

contrib/vexecutor/vexecutor.c
@@ -192,7 +192,7 @@ static TupleTableSlot* VExecProcNode(PlanState *node)
 #endif
         case T_AppendOnlyScanState:
         case T_TableScanState:
-            result = ExecTableVScan((ScanState*)node);
+			result = ExecTableVScanVirtualLayer((ScanState*)node);
             break;
         default:
             break;