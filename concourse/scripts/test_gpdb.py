1c3460ec7ed88de4b997324b6500bbaa6c06b8e7
Taylor Vesely <tvesely@pivotal.io>
2019-01-05 00:03:38 +0000

    WIP: Disable zstd on ubuntu's test_gpdb.py
    Specifically for the configure to run after gpdb is already built.
    
    Co-authored-by: Taylor Vesely <tvesely@pivotal.io>
    Co-authored-by: Karen Huddleston <khuddleston@pivotal.io>

--- concourse/scripts/test_gpdb.py
+++ concourse/scripts/test_gpdb.py
@@ -53,6 +53,8 @@ def configure():
                             "--with-perl",
                             "--with-libxml",
                             "--with-python",
+                            # TODO: Remove this flag after zstd is bundled with our Docker images
+                            "--without-zstd",
                             "--with-libs=/usr/local/gpdb/lib",
                             "--with-includes=/usr/local/gpdb/include",
                             "--prefix=/usr/local/gpdb"], env=p_env, shell=True, cwd="gpdb_src")
